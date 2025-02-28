162d8218b3cb20d9ed80f89e19d956783d799d46
Alejandro Butron <butron@truepath.ai>
2025-02-27 20:03:28 -0600

    Make memory more efficient

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main():
     
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
